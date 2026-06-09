24dd1290e98e4ccf94e98fa833fb951cfa7e49c3
Yokeshwaran-K <yokeshwaranyoki001@gmail.com>
2026-06-09 16:36:06 +0530

    Implement user registration API with Prisma

--- web/app/api/auth/register/route.ts
+++ web/app/api/auth/register/route.ts
@@ -1,19 +1,35 @@
 import { NextResponse } from "next/server";
-
-export async function GET() {
-    return NextResponse.json({
-        message: "Joblynk Register API Working 🚀"
-    });
-}
+import { prisma } from "@/app/lib/prisma";
 
 export async function POST(request: Request) {
-
+  try {
     const body = await request.json();
 
-    return NextResponse.json({
-        success: true,
-        message: "Data received successfully",
-        data: body
+    const user = await prisma.user.create({
+      data: {
+        name: body.name,
+        email: body.email,
+        phone: body.phone,
+        password: body.password,
+        role: body.role,
+      },
     });
 
+    return NextResponse.json({
+      success: true,
+      message: "User registered successfully",
+      user,
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Something went wrong",
+      },
+      { status: 500 }
+    );
+  }
 }
