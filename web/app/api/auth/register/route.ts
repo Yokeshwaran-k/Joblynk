d5d73557b870dc7a2ea69b39ca1c68978d456d7f
Yokeshwaran-K <yokeshwaranyoki001@gmail.com>
2026-06-11 18:30:27 +0530

    Implement password hashing with bcrypt

--- web/app/api/auth/register/route.ts
+++ web/app/api/auth/register/route.ts
@@ -1,25 +1,70 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/app/lib/prisma";
+import bcrypt from "bcrypt"
 
 export async function POST(request: Request) {
   try {
     const body = await request.json();
 
+    // Required field validation
+    if (
+      !body.name ||
+      !body.email ||
+      !body.phone ||
+      !body.password ||
+      !body.role
+    ) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "All fields are required",
+        },
+        { status: 400 }
+      );
+    }
+
+    // Check duplicate email or phone
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        OR: [
+          { email: body.email },
+          { phone: body.phone },
+        ],
+      },
+    });
+
+    if (existingUser) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Email or phone already exists",
+        },
+        { status: 400 }
+      );
+    }
+    const hashedPassword = await bcrypt.hash(body.password, 10);
+    // Create user
     const user = await prisma.user.create({
       data: {
         name: body.name,
         email: body.email,
         phone: body.phone,
-        password: body.password,
+        password: hashedPassword,
         role: body.role,
       },
     });
 
     return NextResponse.json({
-      success: true,
-      message: "User registered successfully",
-      user,
-    });
+  success: true,
+  message: "User registered successfully",
+  data: {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    phone: user.phone,
+    role: user.role,
+  },
+});
 
   } catch (error) {
     console.error(error);
